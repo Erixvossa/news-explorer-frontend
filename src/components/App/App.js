dae646890efdb60b8e3b4770a9660047a432a531
Alex Slomyanovskiy <erixvossa@yandex.ru>
2020-10-31 15:13:45 +0300

    feature/images_constants

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, { useCallback, useRef } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+
+import './App.css';
+
+import { searchResExample, NewsExample } from '../../utils/constants';
+
+
+
+function App(props) {
+  const history = useHistory();
+
+
+  return (
+      <div className="page">
+
+      </div>
+  )
+}
+
+export default App;
