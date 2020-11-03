1256df019fa38393674337b66d28fac01cfdd2bf
Alex Slomyanovskiy <erixvossa@yandex.ru>
2020-11-03 10:13:59 +0300

    feature/media

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,10 @@ import { Switch, Route, useHistory } from 'react-router-dom';
 import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
+import SavedNews from '../SavedNews/SavedNews';
 import Footer from '../Footer/Footer';
+import LoginPopup from '../LoginPopup/LoginPopup';
+import RegisterPopup from '../RegisterPopup/RegisterPopup';
 import { searchResExample, NewsExample } from '../../utils/constants';
 
 
@@ -12,12 +15,114 @@ import { searchResExample, NewsExample } from '../../utils/constants';
 function App(props) {
   const history = useHistory();
 
+    const [activePage, setActivePage] = React.useState('main');
+    const [loggedIn, setLoggedIn] = React.useState(false);
+    const [searchQueryResults, setSearchQueryResults] = React.useState(searchResExample);
+    const [savedNews, setSavedNews] = React.useState(NewsExample);
+    const [mobileMenuOpened, setMobileMenuOpened] = React.useState(false);
+    const [loginPopupOpened, setLoginPopupOpened] = React.useState(false);
+    const [registerPopupOpened, setRegisterPopupOpened] = React.useState(false);
+    const loginPopupOpenedRef = useRef();
+    loginPopupOpenedRef.current = loginPopupOpened;
 
-  return (
-      <div className="page">
+    function setMainPageActive() {
+        setActivePage('main');
+    }
 
-      </div>
-  )
+    function setSavedNewsPageActive() {
+        setActivePage('saved-news');
+    }
+
+    function toggleMobileMenu() {
+        setMobileMenuOpened(!mobileMenuOpened);
+    }
+
+    function onKeydown(evt) {
+        if (evt.key === 'Escape') {
+            loginPopupOpenedRef.current ? closeLoginPopup() : closeRegisterPopup();
+        }
+    }
+
+    const memoizedOnKeydown = useCallback(onKeydown, []);
+
+    function onOverlayClick(evt) {
+        if (evt.target === evt.currentTarget) {
+            loginPopupOpenedRef.current ? closeLoginPopup() : closeRegisterPopup();
+        }
+    }
+
+    const memoizedOnOverlayClick = useCallback(onOverlayClick, []);
+
+    function openLoginPopup() {
+        document.addEventListener('keydown', memoizedOnKeydown);
+        document.querySelector('.popup_type_login').addEventListener('click', memoizedOnOverlayClick);
+        setLoginPopupOpened(true);
+    }
+
+    function closeLoginPopup() {
+        setLoginPopupOpened(false);
+        document.querySelector('.popup_type_login').removeEventListener('click', memoizedOnOverlayClick);
+        document.removeEventListener('keydown', memoizedOnKeydown);
+    }
+
+    function openRegisterPopup() {
+        document.addEventListener('keydown', memoizedOnKeydown);
+        document.querySelector('.popup_type_register').addEventListener('click', memoizedOnOverlayClick);
+        setRegisterPopupOpened(true);
+    }
+
+    function closeRegisterPopup() {
+        setRegisterPopupOpened(false);
+        document.querySelector('.popup_type_register').removeEventListener('click', memoizedOnOverlayClick);
+        document.removeEventListener('keydown', memoizedOnKeydown);
+    }
+
+    function popupChange() {
+        if (loginPopupOpened) {
+            closeLoginPopup();
+            openRegisterPopup();
+        } else {
+            closeRegisterPopup();
+            openLoginPopup();
+        }
+    }
+
+    function handleLogout() {
+        setMainPageActive();
+        setLoggedIn(false);
+        history.push('/');
+    }
+
+    return (
+        <div className="page">
+            <Header
+                activePage={activePage}
+                setMainPageActive={setMainPageActive}
+                setSavedNewsPageActive={setSavedNewsPageActive}
+                loggedIn={loggedIn}
+                mobileMenuOpened={mobileMenuOpened}
+                toggleMobileMenu={toggleMobileMenu}
+                openLoginPopup={openLoginPopup}
+                handleLogout={handleLogout}
+            />
+            <Switch>
+                <Route path="/" exact>
+                    <Main
+                        activePage={activePage}
+                        searchQueryResults={searchQueryResults}
+                        loggedIn={loggedIn}
+                    />
+                </Route>
+                <Route path="/saved-news">
+                    <SavedNews setSavedNewsPageActive={setSavedNewsPageActive} savedNews={savedNews} />
+                </Route>
+            </Switch>
+            <Footer setMainPageActive={setMainPageActive} />
+
+            <LoginPopup loginPopupOpened={loginPopupOpened} onClose={closeLoginPopup} onLinkClick={popupChange} />
+            <RegisterPopup registerPopupOpened={registerPopupOpened} onClose={closeRegisterPopup} onLinkClick={popupChange} />
+        </div>
+    )
 }
 
 export default App;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import './Main.css';
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
 import NewsCardList from '../NewsCardList/NewsCardList';
