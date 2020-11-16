5a30fb55ea85ebd86fdeca68aeb3f574fc313997
Alex Slomyanovskiy <erixvossa@yandex.ru>
2020-11-16 10:41:41 +0300

    level-3

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,3 @@
-import card1 from '../images/card1.png';
-import card2 from '../images/card2.png';
-import card3 from '../images/card3.png';
-
-
-
-
-
 
 export const BASE_URL = 'https://api.ervossa.students.nomoreparties.co';
 export const SERVER_ERROR_MESSAGE = 'Во время запроса произошла ошибка. Попробуйте ещё раз через некоторое время.';
