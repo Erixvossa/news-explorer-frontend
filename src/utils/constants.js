b69bee0e0e9fbc9ef3383d0e043be9f0396c9615
Alex Slomyanovskiy <erixvossa@yandex.ru>
2020-11-16 10:05:38 +0300

    feature/refactor

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,6 +7,6 @@ import card3 from '../images/card3.png';
 
 
 
-export const BASE_URL = 'https://ervossa.students.nomoreparties.co/api';
+export const BASE_URL = 'https://api.ervossa.students.nomoreparties.co';
 export const SERVER_ERROR_MESSAGE = 'Во время запроса произошла ошибка. Попробуйте ещё раз через некоторое время.';
 export const API_KEY = '5857ffe25c2d4c608d943c644811ed07';
